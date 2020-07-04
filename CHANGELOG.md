0f5980e48367415b27ad4903bcf92a587f4cdb4d
Damian Zyngier <damian.zyngier@gmail.com>
2020-07-04 09:26:30 +0200

    Added auto checkbox.

---
+++ CHANGELOG.md
@@ -0,0 +1,24 @@
+# Changelog
+
+All notable changes to this project will be documented in this file.
+
+## [1.3] - 2020-07-04
+
+- Added checkbox to automatically apply clear all 0,00's, and validate all input and result fields.
+- Changed 'Validate' to also work with weekend days.
+
+## [1.2] - 2020-07-02
+
+- Changed 'Validate sum' to 'Validate' button. Now it will validate non-weekend inputs if they are above maximum value or not dividable by 0,5.
+
+## [1.1] - 2020-07-01
+
+
+- Changed 'Validate sum' for result to check result if maximum value has been reached, or if it is dividable by 0,5.
+- Added LICENSE.md.
+
+## [1.0] - 2020-06-30
+
+- Added 'Clear all 0,00' button.
+- Added 'Validate sum' button.
+- Added README.md.
--- fixppm.js
+++ fixppm.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         More readable PPM
 // @namespace    https://itg.crifnet.com/itg/tm/EditTimeSheet.do?timesheetId=*
-// @homepage https://github.com/DamianZyngier/FixPPM/
-// @homepageURL https://github.com/DamianZyngier/FixPPM/
-// @updateURL https://github.com/DamianZyngier/FixPPM/blob/master/fixppm.js
-// @downloadURL https://github.com/DamianZyngier/FixPPM/blob/master/fixppm.js
-// @version      1.2
+// @homepage     https://github.com/DamianZyngier/FixPPM/
+// @homepageURL  https://github.com/DamianZyngier/FixPPM/
+// @updateURL    https://raw.githubusercontent.com/DamianZyngier/FixPPM/master/fixppm.js
+// @downloadURL  https://raw.githubusercontent.com/DamianZyngier/FixPPM/master/fixppm.js
+// @version      1.3
 // @description  Finally, I see where to report!
 // @author       Damian Zyngier
 // @match        https://itg.crifnet.com/itg/tm/EditTimeSheet.do?timesheetId=*
@@ -13,7 +13,7 @@
 
 (function() {
     'use strict';
-	
+
 	var maxValue = 8;
 	var maxValueString = "8,00";
 	
@@ -23,19 +23,46 @@
 	var textColorSunday = "white"
 	var bgColorWarning = "orange"
 	var bgColorDefault = "white"
-
-    function createButton(context, func, value) {
-        var button = document.createElement("input");
-        button.type = "button";
-        button.value = value;
-        button.onclick = func;
-        button.style.padding = "5px";
-        button.style.marginLeft = "10px";
-        button.style.fontSize = "15px";
-
-        context.appendChild(button);
-    }
+	var bgColorCorrect = "lightgreen"
 	
+	function createButton(context, func, value) {
+		var button = document.createElement("input");
+		button.type = "button";
+		button.value = value;
+		button.onclick = func;
+		button.style.padding = "5px";
+		button.style.marginLeft = "10px";
+		button.style.fontSize = "15px";
+		button.style.verticalAlign  = "middle";
+		
+		context.appendChild(button);
+	}
+
+	function createCheckbox(context, func, labelValue) {
+		var checkbox = document.createElement("input");
+		checkbox.type = "checkbox";
+		checkbox.onclick = func;
+		checkbox.id = "fixPpmAuto";
+		checkbox.style.padding = "5px";
+		checkbox.style.marginLeft = "10px";
+		checkbox.style.fontSize = "15px";
+		checkbox.style.verticalAlign  = "middle";
+		checkbox.style.zoom  = "1.5";
+		
+		var label = document.createElement("label");
+		label.innerHTML = labelValue;
+		label.style.verticalAlign  = "middle";
+		label.style.padding = "2px";
+		label.style.fontSize = "15px";
+		
+		context.appendChild(checkbox);
+		context.appendChild(label);
+		
+		if (localStorage.getItem("isFixPpmAutoChecked") == "true") {
+			checkbox.click();
+		}
+	}
+
     function clearZeros() {
 		var allInputs = document.getElementsByTagName("input");
 		for (i = 0; i < allInputs.length; i++) {
@@ -49,47 +76,77 @@
 		}
 	}
 	
-	function validate() {
-		colorTdsInTable('#table7');
+	function validateAllFields() {
+		var value;
+		var inputsTable = document.querySelector("#table4");
+		var inputNodes = inputsTable.querySelectorAll("input");
+		for (i = 0; i < inputNodes.length; i++) {
+			if(typeof inputNodes[i] == "undefined" || inputNodes[i].type == "hidden") {
+				continue;
+			}
+			
+			validateAndColorInput(inputNodes[i]);
+		}
+		
+		validateResults();
+	}
+	
+	function validateInputEvent() {
+		if (this.value == "0,00") {
+			this.value = "";
+			colorDefaultInput(this.parentNode);
+		} else {
+			validateAndColorInput(this);
+		}
+		validateResults();
+	}
+	
+	function validateAndColorInput(object) {
+			console.log(object.value);
+		
+		var nodeValue = parseFloat(object.value.replace(",","."));
+		
+		if (!isNaN(nodeValue) && (object.value.length > 4 || nodeValue % 0.5 != 0 || nodeValue > maxValue )) {
+			object.parentNode.style.backgroundColor = bgColorWarning;
+		} else {
+			colorDefaultInput(object.parentNode);
+		}
+	}
+	
+	function validateResults() {
+		var nodeValue;
 		var resultTable = document.querySelector('#table7');
 		var resultNodes = resultTable.querySelectorAll("span");
 		for (i = 0; i < resultNodes.length; i++) {
-			if(typeof resultNodes[i] == "undefined" || resultNodes[i].parentNode.style.backgroundColor == bgColorSaturday || resultNodes[i].parentNode.style.backgroundColor == bgColorSunday) {
+			if(typeof resultNodes[i] == "undefined") {
 				continue;
 			}
-			
-			if (resultNodes[i].innerHTML == maxValueString) {
-				resultNodes[i].parentNode.style.backgroundColor = "lightgreen";
-			} else if (parseFloat(resultNodes[i].innerHTML.replace(",",".")) > maxValue || parseFloat(resultNodes[i].innerHTML.replace(",",".")) % 0.5 != 0) {
+			nodeValue = resultNodes[i].innerHTML;
+			if (nodeValue == maxValueString) {
+				resultNodes[i].parentNode.style.backgroundColor = bgColorCorrect;
+			} else if (nodeValue.length > 10 || parseFloat(nodeValue.replace(",",".")) > maxValue || parseFloat(nodeValue.replace(",",".")) % 0.5 != 0) {
 				resultNodes[i].parentNode.style.backgroundColor = bgColorWarning;
 			} else {
-				resultNodes[i].parentNode.style.backgroundColor = bgColorDefault;
+				colorDefaultInput(resultNodes[i].parentNode);
 			}
 		}
-		
-		var value;
-		var inputsTable = document.querySelector('#table4');
-		var inputNodes = inputsTable.querySelectorAll("input");
-		for (i = 0; i < inputNodes.length; i++) {
-			if(typeof inputNodes[i] == "undefined" || inputNodes[i].type == "hidden" || inputNodes[i].parentNode.style.backgroundColor == bgColorSaturday || inputNodes[i].parentNode.style.backgroundColor == bgColorSunday) {
-				continue;
-			}
-			
-			value = parseFloat(inputNodes[i].value.replace(",","."));
-			if (!isNaN(value) && (value % 0.5 != 0 || value > maxValue)) {
-				console.log(inputNodes[i].value);
-				inputNodes[i].parentNode.style.backgroundColor = bgColorWarning
-			} else {
-				inputNodes[i].parentNode.style.backgroundColor = bgColorDefault;
-			}
-		}
-		
 	}
 	
-	function colorTdsInTable(tableId) {	
+	function colorDefaultInput(object) {
+		if (object.classList.contains("sat")) {
+			object.style.backgroundColor = bgColorSaturday;
+		} else if (object.classList.contains("sun")) {
+			object.style.backgroundColor = bgColorSunday;
+		} else {
+			object.style.backgroundColor = bgColorDefault;
+		}
+	}
+	
+	
+	function colorAllTdsInTable(tableId) {
 		var mainTable = document.querySelector(tableId);
 		var trSelectors = mainTable.querySelectorAll('tr');
-		var tdSelectors, j;
+		var tdSelectors, i, j;
 		for (i = 0; i < trSelectors.length; i++) {
 			if(typeof trSelectors[i] == "undefined") {
 				continue;
@@ -104,48 +161,75 @@
 				if (saturdays.includes(j)) {
 					tdSelectors[j].style.backgroundColor = bgColorSaturday;
 					tdSelectors[j].style.color = textColorSaturday;
+					tdSelectors[j].classList.add("sat");
 				}
 				
 				if (sundays.includes(j)) {
 					tdSelectors[j].style.backgroundColor = bgColorSunday;
 					tdSelectors[j].style.color = textColorSunday;
+					tdSelectors[j].classList.add("sun");
 				}
 			}
 		}
 	}
 	
-    createButton(document.body, clearZeros, "Clear all 0,00");
-    createButton(document.body, validate, "Validate");
+	function checkTheBox () {
+		clearZeros();
+		validateAllFields();
 		
-	clearZeros();
-
-	var i;
-	var x = document.getElementById("table1");
+		var isChecked = document.getElementById("fixPpmAuto").checked;
+		localStorage.setItem("isFixPpmAutoChecked", isChecked);
+		
+		var inputsTable = document.querySelector('#table4');
+		var inputNodes = inputsTable.querySelectorAll("input");
+		for (i = 0; i < inputNodes.length; i++) {
+			if(typeof inputNodes[i] == "undefined" || inputNodes[i].type == "hidden") {
+				continue;
+			}
+			if (isChecked) {
+				inputNodes[i].addEventListener("change", validateInputEvent);
+			} else {	
+				inputNodes[i].removeEventListener("change", validateInputEvent);
+			}
+		}
+	}
 	
-	var headerTable = document.querySelector('#table1');
-	var headerNodes = headerTable.querySelectorAll('.ellipsisCell');
+	function colorHeaderRow() {	
+		var headerTable = document.querySelector('#table1');
+		var headerNodes = headerTable.querySelectorAll('.ellipsisCell');
+		
+		for (var i = 0; i < headerNodes.length; i++) {	
+			if(typeof headerNodes[i] == "undefined") {
+				continue;
+			}
+			
+			if (headerNodes[i].innerHTML.includes("So<br>")) {
+				headerNodes[i].style.backgroundColor = bgColorSaturday;
+				headerNodes[i].style.color = textColorSaturday;
+				saturdays[saturdays.length] = i;
+			}
+			
+			if (headerNodes[i].innerHTML.includes("N<br>")) {
+				headerNodes[i].style.backgroundColor = bgColorSunday;
+				headerNodes[i].style.color = textColorSunday;
+				sundays[sundays.length] = i;
+			}
+		}
+	}
+	
+	var x = document.getElementById("table1");
 	
 	var saturdays = [];
 	var sundays = [];
 	
-	for (i = 0; i < headerNodes.length; i++) {	
-		if(typeof headerNodes[i] == "undefined") {
-			continue;
-		}
-		
-		if (headerNodes[i].innerHTML.includes("So<br>")) {
-			headerNodes[i].style.backgroundColor = bgColorSaturday;
-			headerNodes[i].style.color = textColorSaturday;
-			saturdays[saturdays.length] = i;
-		}
-		
-		if (headerNodes[i].innerHTML.includes("N<br>")) {
-			headerNodes[i].style.backgroundColor = bgColorSunday;
-			headerNodes[i].style.color = textColorSunday;
-			sundays[sundays.length] = i;
-		}
-	}
+	colorHeaderRow();
 	
-	colorTdsInTable('#table4');
-	colorTdsInTable('#table7');
+    createButton(document.body, clearZeros, "Clear all 0,00");
+    createButton(document.body, validateAllFields, "Validate");
+    createCheckbox(document.body, checkTheBox, "Auto clear & validate");
+	
+	clearZeros();
+	
+	colorAllTdsInTable("#table4");
+	colorAllTdsInTable("#table7");
 })();
